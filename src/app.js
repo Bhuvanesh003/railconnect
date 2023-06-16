6d2dbf42b847460c9f3f00af6a1886496c3a63f2
Bhuvanesh R <bhuviram03@gmail.com>
2023-06-16 15:20:04 +0530

    sixth commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const bp = require('body-parser');
+const express=require('express');
+const app=express();
+const userroute=require('../route/user')
+const path=require('path')
+const { engine }=require('express-handlebars')
+app.use(bp.urlencoded());
+
+app.use(express.static(path.join(__dirname,'public')));
+app.engine('hbs',engine({extname:'.hbs',defaultLayout:false}));
+app.set('view engine','hbs');
+app.set('views','views');
+app.use(userroute);
+app.listen(3000,()=>{
+    console.log("HII");
+});
